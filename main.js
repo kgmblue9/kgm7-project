3041c46411cc4e11ef7e96a3e4db73ac5eb8fc28
Gyuman Kim <kgmblue9@gmail.com>
2024-02-20 17:16:09 +0900

    2nd commit

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const API_KEY = `40b8bde5eada4aa9a2884a3e8ba42c2b`;
 let news = [];
-const getLatestNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+const getLatestNews = async ()=>{
+    // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
+    console.log(url);
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
     console.log(news);
-}
-getLatestNews()
+} 
+getLatestNews();
