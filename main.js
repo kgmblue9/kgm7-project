b1a21dcedab16b62ed660c4313125d186a6c8269
Gyuman Kim <kgmblue9@gmail.com>
2024-02-20 17:21:22 +0900

    3rd commit

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const API_KEY = `40b8bde5eada4aa9a2884a3e8ba42c2b`;
 let news = [];
 const getLatestNews = async ()=>{
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
+    // const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    const url = new URL(`https://kgm7.netlify.app/top-headlines`);    
+
     console.log(url);
     const response = await fetch(url);
     const data = await response.json();
