a186e51b43a9312762050af09788387dd73d9ee7
Gyuman Kim <kgmblue9@gmail.com>
2024-02-22 03:49:00 +0900

    render modify

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let noImage = "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
 const API_KEY = `40b8bde5eada4aa9a2884a3e8ba42c2b`;
 let newsList = [];
 const getLatestNews = async ()=>{
@@ -14,18 +15,23 @@ const getLatestNews = async ()=>{
     const render = ()=>{
         const newsHTML = newsList.map((news) => `<div class = "row news">
         <div class = "col-lg-4">
-            <img class = "news-img-size" src="${news.urlToImage}">
+            <img class = "news-img-size" src="${news.urlToImage??noImage}">
         </div>
         <div class = "col-lg-8">
             <h2>${news.title}</h2>
-            <p>${news.description}</P>
-            <div>${news.source.name}, ${news.publishedAt}</div>
+            <p>${news.description??"내용없음"}</P>
+            <div>${news.source.name??"no source"} , ${moment(news.publishedAt).fromNow()}</div>
         </div>
     </div>`
     ).join('');
         document.getElementById('news-board').innerHTML = newsHTML;
     }
     render();
+
+
+    moment().startOf('hour').fromNow();  
+
+
 }   
 getLatestNews();
 
